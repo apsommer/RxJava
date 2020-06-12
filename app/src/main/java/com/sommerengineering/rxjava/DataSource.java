319a96ee542e923246173d263fdbaaffc3e90024
Drew Sommer <drew@sommerengineering.com>
2020-06-12 15:50:37 -0700

    feat: Extend data model to list

---
+++ app/src/main/java/com/sommerengineering/rxjava/DataSource.java
@@ -0,0 +1,22 @@
+package com.sommerengineering.rxjava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create a list of tasks.
+ */
+public class DataSource {
+
+    public static List<Task> createTaskList() {
+
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("Eat lunch", true, 3));
+        tasks.add(new Task("Wash dishes", true, 2));
+        tasks.add(new Task("Learn RxJava", false, 1));
+        tasks.add(new Task("Go to sleep", false, 4));
+
+        return tasks;
+    }
+
+}
